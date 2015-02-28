2969e9a2a81a3f6573b44d7c4dc8109d486df3fd
Karel Fiala <fiala.karel@gmail.com>
2015-02-28 21:50:29 +0100

    rebase main to main+trivial

--- main.cpp
+++ main.cpp
@@ -28,20 +28,12 @@ typedef struct {
 } Coord;
 */
 
+#include "trivial.cpp"
+
 void mainProccesLoop() {
-    for (int i = 0; i < matA_m; i++) {
-        for (int j = 0; j < matB_p; j++) {
-            int result = 0;
-            
-            for (int k = 0; k < matA_n; k++) {
-                    result += matA[i][k] * matB[k][j];
-                    //cout << result << " " << matA[i][k] << " " <<  matB[k][j] << endl << flush;
-            }
-            
-            matC[i][j] = result;
-        }
-    }
+    trivial();
 }
+
 
 void debugMatrix() {
     cout << endl << "matA: " << endl;
